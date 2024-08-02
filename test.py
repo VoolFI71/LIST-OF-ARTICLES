c2ab431e4f96f08910fae4bbf3352a42e9d7b9b3
Gleb <gleb.gleb.tula@mail.ru>
2024-08-02 17:50:01 +0300

    fix gitignore

--- test.py
+++ test.py
@@ -4,15 +4,3 @@ import sqlite3
 import jwt
 import time
 from config import secret_key
-import hashlib
-router_auth = APIRouter()
-
-def hash_password(password: str) -> str:
-    sha256 = hashlib.sha256()
-
-    sha256.update((password+"salt").encode('utf-8'))
-
-    hashed_password = sha256.hexdigest()
-
-    return hashed_password
-print(hash_password("1d"))
