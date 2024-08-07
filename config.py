5e2b193855ae458ea75e12e6da5bc4843ed35cae
Gleb <gleb.gleb.tula@mail.ru>
2024-08-07 14:37:36 +0300

    ускорил подсчёт количества авторизованных людей на сайте

--- config.py
+++ config.py
@@ -15,18 +15,24 @@ DB_PASSWORD="1234"
 
 class ConnectionManager:
     def __init__(self) -> None:
-        self.active_connections: dict[str, WebSocket] = []
-        self.active_tokens: list[str] = []
+        self.active_connections: list[WebSocket] = []
+        self.active_nicks2: dict[str, int] = {}
+
     async def connect(self, token: str, websocket: WebSocket):
         payload = jwt.decode(token, secret_key, algorithms=['HS256'])
         self.active_connections.append(websocket)
-        self.active_tokens.append(payload["sub"])
+        if payload["sub"] not in self.active_nicks2:
+            self.active_nicks2[payload["sub"]] = 0
+        self.active_nicks2[payload["sub"]] += 1
         await self.broadcast_user_count()
 
     async def disconnect(self, token, websocket: WebSocket):
         self.active_connections.remove(websocket)
         payload = jwt.decode(token, secret_key, algorithms=['HS256'])
-        self.active_tokens.remove(payload["sub"])
+        if payload["sub"] in self.active_nicks2:
+            self.active_nicks2[payload["sub"]] -= 1
+            if self.active_nicks2[payload["sub"]] == 0:
+                del self.active_nicks2[payload["sub"]]
         await self.broadcast_user_count()
 
     async def send_personal_message(self, message: str, websocket: WebSocket):
@@ -37,6 +43,7 @@ class ConnectionManager:
             await connection.send_text(data)
 
     async def broadcast_user_count(self):
-        count = len(set(self.active_tokens))
-        message = json.dumps({"user_count": count})
+        count2 = [1 for count_name in self.active_nicks2 if self.active_nicks2[count_name] >= 1]
+        print(count2)
+        message = json.dumps({"user_count": len(count2)})
         await self.broadcast(message)
